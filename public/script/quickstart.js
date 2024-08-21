f6a7c007f232bb81bc0b70c5079755c310216e8e
Andreas Stassivik <andreas@stassi.net>
2024-08-21 15:51:07 -0700

    OpenStreetMap tile layer added to map

--- public/script/quickstart.js
+++ public/script/quickstart.js
@@ -1,3 +1,6 @@
-(({ map: selectMap }) => {
-  selectMap('map').setView([51.505, -0.09], 13);
+(({ map: createMap, tileLayer }) => {
+  tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>',
+    maxZoom: 19
+  }).addTo(createMap('map', { center: [51.505, -0.09], zoom: 13 }))
 })(window.leaflet)
